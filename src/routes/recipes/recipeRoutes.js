b0651a238288882a59b505a5f49b7156fd90ee28
Olga Kopeć <olga.kopec@gmail.com>
2024-08-03 17:09:25 +0200

    podmianka w folderach

--- src/routes/recipes/recipeRoutes.js
+++ src/routes/recipes/recipeRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { getRecipes, getRecipeById, createRecipe } = require('../../controllers/recipes/recipeController');
-const authMiddleware = require('../../middleware/auth');
+// const authMiddleware = require('../../middleware/auth');
 
 const router = express.Router();
 
-router.get('/', authMiddleware, getRecipes);
-router.get('/:id', authMiddleware, getRecipeById);
-router.post('/', authMiddleware, createRecipe);
+// router.get('/', authMiddleware, getRecipes);
+// router.get('/:id', authMiddleware, getRecipeById);
+// router.post('/', authMiddleware, createRecipe);
+
+router.get('/', getRecipes);
+router.get('/:id', getRecipeById);
+router.post('/', createRecipe);
 
 module.exports = router;

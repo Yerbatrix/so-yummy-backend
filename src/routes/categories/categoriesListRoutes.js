37c6bc044e07a22a6450f8dd11b00ca50fcfbbf1
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-08-04 18:31:36 +0200

    swagger-category&subscribe

--- src/routes/categories/categoriesListRoutes.js
+++ src/routes/categories/categoriesListRoutes.js
@@ -3,6 +3,36 @@ const router = express.Router();
 
 const categoriesController = require("../../controllers/categories/categoriesController");
 
+/**
+ * @swagger
+ * /api/categories:
+ *   get:
+ *     summary: Get list of categories
+ *     tags: [Categories]
+ *     responses:
+ *       200:
+ *         description: List of categories
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: success
+ *                 code:
+ *                   type: integer
+ *                   example: 200
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     categories:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *       500:
+ *         description: Server error
+ */
 router.get("/", categoriesController.get);
 
 module.exports = router;
--- src/routes/subscribe/subscribeRoutes.js
+++ src/routes/subscribe/subscribeRoutes.js
@@ -3,6 +3,32 @@ const router = express.Router();
 
 const subscribeController = require("../../controllers/subscribe/subscribeController");
 
+/**
+ * @swagger
+ * /api/subscribe:
+ *   post:
+ *     summary: Subscribe to newsletter
+ *     tags: [Subscribe]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The user's email
+ *     responses:
+ *       200:
+ *         description: Subscription email sent
+ *       400:
+ *         description: Missing required field email
+ *       404:
+ *         description: You are already subscribed to our newsletter
+ */
 router.post("/", subscribeController.subscription);
 
 module.exports = router;

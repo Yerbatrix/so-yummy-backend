d6958e3e98ca944a6f5851b674d363b9458f4a9e
MateuszBieda <mateusbieda@gmail.com>
2024-08-06 00:42:37 +0200

    update authService file

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,85 +1,24 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const authService = require("../../services/authService");
+const { User } = require("../models/User");
 
-// signup
-exports.register = async (req, res, next) => {
-  const { name, email, password } = req.body;
+const checkEmailAddress = async (email) => {
+  return await User.findOne({ email });
+};
 
-  try {
-    let existingUser = await authService.checkEmailAddress(email);
-    if (existingUser) {
-      return res.status(409).json({ msg: "Email in use" });
+const getCurrentUser = async (id) => {
+  return await User.findById(id).select("-password -jwtToken");
+};
+
+const updateUserData = async (id, updates) => {
+  return User.findByIdAndUpdate(
+    id,
+    { $set: updates },
+    {
+      new: true,
     }
-    const newUser = new User({
-      name,
-      email,
-    });
-    newUser.setPassword(password);
-    await newUser.save();
-    res.status(201).json({
-      user: {
-        name,
-        email,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
+  ).select("-password -jwtToken");
 };
-
-// signin
-exports.signin = async (req, res, next) => {
-  const { email, password } = req.body;
-  try {
-    const user = await authService.checkEmailAddress(email);
-    if (!user || !user.validPassword(password)) {
-      return res.status(401).json({ msg: "Email or password is wrong" });
-    }
-    const payload = { id: user._id, email: user.email };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    user.jwtToken = token;
-    await user.save();
-    res.json({
-      token,
-      user: {
-        email: user.email,
-        subscription: user.subscription,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-exports.getUserInfo = async (req, res, next) => {
-  try {
-    const user = await authService.getCurrentUser(req.user.id);
-    res.json(user);
-  } catch (err) {
-    next(err);
-  }
-};
-
-exports.updateUserInfo = async (req, res, next) => {
-  try {
-    const updates = req.body;
-    const user = await authService.updateUserData(req.user.id, updates);
-    res.json(user);
-  } catch (err) {
-    next(err);
-  }
-};
-
-exports.logout = async (req, res, next) => {
-  try {
-    const user = await authService.getCurrentUser(req.user.id);
-    user.jwtToken = null;
-    await user.save();
-    res.json({ msg: "Logged out successfully" });
-  } catch (err) {
-    next(err);
-  }
+module.exports = {
+  checkEmailAddress,
+  getCurrentUser,
+  updateUserData,
 };
